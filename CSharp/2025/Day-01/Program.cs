90fdf8825083a0a0c70816b29979913cb197bcc5
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-01 08:16:08 +0100

    feat: Add structure of aoc 2025 Day 1

---
+++ CSharp/2025/Day-01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Advent of Code 2025: Day 1
+
+var lines = File.ReadAllLines("input.txt");
+
+System.Console.WriteLine($"Part 1: {Part1(lines)}");
+System.Console.WriteLine($"Part 2: {Part2(lines)}");
+
+int Part1(string[] lines)
+{
+    return 0;
+}
+
+
+int Part2(string[] lines)
+{
+    return 0;
+}
