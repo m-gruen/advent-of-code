4cfcde80916ca3707e50a533d3a1fed6274d3692
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-01 09:11:06 +0100

    feat: Implement Part 2 logic for Advent of Code 2025 Day 1

--- CSharp/2025/Day-01/Program.cs
+++ CSharp/2025/Day-01/Program.cs
@@ -37,8 +37,39 @@ int Part1(string[] lines)
     return zeroCount;
 }
 
-
 int Part2(string[] lines)
 {
-    return 0;
+    int currentNumber = 50;
+    int zeroCount = 0;
+
+    foreach (var line in lines)
+    {
+        char direction = line[0];
+        int dist = int.Parse(line[1..]);
+
+        zeroCount += dist / 100;
+
+        int remainingSteps = dist % 100;
+
+        for (int i = 0; i < remainingSteps; i++)
+        {
+            if (direction == 'L')
+            {
+                currentNumber--;
+                if (currentNumber < 0) { currentNumber = 99; }
+            }
+            else
+            {
+                currentNumber++;
+                if (currentNumber == 100) { currentNumber = 0; }
+            }
+
+            if (currentNumber == 0)
+            {
+                zeroCount++;
+            }
+        }
+    }
+
+    return zeroCount;
 }
