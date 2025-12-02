5b5a533b28ba2bde0c6222d6dfecb2440c10dcc6
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-02 11:01:39 +0100

    feat: Update input handling for Advent of Code 2025 Day 2 and implement Part 1 logic

--- CSharp/2025/Day-02/Program.cs
+++ CSharp/2025/Day-02/Program.cs
@@ -1,16 +1,39 @@
 ﻿// Advent of Code 2025: Day 2
 
-var lines = File.ReadAllLines("input.txt");
+var text = File.ReadAllText("input.txt");
 
-Console.WriteLine($"Part 1: {Part1(lines)}");
-Console.WriteLine($"Part 2: {Part2(lines)}");
+Console.WriteLine($"Part 1: {Part1(text)}");
+Console.WriteLine($"Part 2: {Part2(text)}");
 
-int Part1(string[] lines)
+long Part1(string text)
 {
-    return 0;
+    var idParts = text.Split(',');
+    long invalidIdSum = 0;
+
+    foreach (var idPart in idParts)
+    {
+        var ids = idPart.Split('-').Select(long.Parse).ToArray();
+
+        for (long id = ids[0]; id <= ids[1]; id++)
+        {
+            var idStr = id.ToString();
+            if (idStr.Length % 2 == 0)
+            {
+                var halfLength = idStr.Length / 2;
+                var firstHalf = idStr[..halfLength];
+                var secondHalf = idStr[halfLength..];
+                if (firstHalf == secondHalf)
+                {
+                    invalidIdSum += id;
+                }
+            }
+        }
+    }
+
+    return invalidIdSum;
 }
 
-int Part2(string[] lines)
+int Part2(string text)
 {
     return 0;
 }
