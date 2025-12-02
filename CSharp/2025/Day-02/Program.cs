35d2968e7d3e1e2d87d80ed0d56a04d01517ad4f
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-02 10:18:17 +0100

    feat: Add initial implementation for Advent of Code 2025 Day 2

---
+++ CSharp/2025/Day-02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Advent of Code 2025: Day 2
+
+var lines = File.ReadAllLines("input.txt");
+
+Console.WriteLine($"Part 1: {Part1(lines)}");
+Console.WriteLine($"Part 2: {Part2(lines)}");
+
+int Part1(string[] lines)
+{
+    return 0;
+}
+
+int Part2(string[] lines)
+{
+    return 0;
+}
