02e4b15751a86dc1cc3177391f8ecf24f28d6f33
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-03 10:06:16 +0100

    feat: Implement Part 1 logic for Advent of Code 2025 Day 3

--- CSharp/2025/Day-03/Program.cs
+++ CSharp/2025/Day-03/Program.cs
@@ -7,11 +7,33 @@ Console.WriteLine($"Part 2: {Part2(lines)}");
 
 int Part1(string[] lines)
 {
-    return 0;
+    int sum = 0;
+
+    foreach (var line in lines)
+    {
+        int[] numbers = [.. line.Select(c => c - '0')];
+
+        int max = numbers.Max();
+        int secondMax;
+        int indexOfMax = Array.IndexOf(numbers, max);
+        if (indexOfMax == numbers.Length - 1)
+        {
+            numbers[indexOfMax] = -1;
+            secondMax = numbers.Max();
+            sum += secondMax * 10 + max;
+        }
+        else
+        {
+            secondMax = numbers[(indexOfMax + 1)..].Max();
+            sum += max * 10 + secondMax;
+        }
+    }
+
+    return sum;
 }
 
 int Part2(string[] lines)
 {
-   return 0;
+    return 0;
 }
 
