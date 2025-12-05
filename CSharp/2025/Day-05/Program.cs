744c7cd37d36afd30e313c491f675bfe13817230
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-05 09:58:38 +0100

    feat: Add initial project files and input data for Advent of Code 2025 Day 5

---
+++ CSharp/2025/Day-05/Program.cs
@@ -0,0 +1,16 @@
+﻿// Advent of Code 2025: Day 5
+
+var text = File.ReadAllText("input.txt");
+
+Console.WriteLine($"Part 1: {Part1(text)}");
+Console.WriteLine($"Part 2: {Part2(text)}");
+
+int Part1(string text)
+{
+    return 0;
+}
+
+int Part2(string text)
+{
+    return 0;
+}
