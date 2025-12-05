25bbaa8188f1618eaa174ca88024eeed5a1ddf5d
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-05 09:43:16 +0100

    feat: Implement Part 1 logic for Advent of Code 2025 Day 4

--- CSharp/2025/Day-04/Program.cs
+++ CSharp/2025/Day-04/Program.cs
@@ -7,7 +7,46 @@ Console.WriteLine($"Part 2: {Part2(lines)}");
 
 int Part1(string[] lines)
 {
-    return 0;
+    int count = 0;
+    int rows = lines.Length;
+    int cols = lines[0].Length;
+    
+    int[] dx = [-1, -1, -1, 0, 0, 1, 1, 1];
+    int[] dy = [-1, 0, 1, -1, 1, -1, 0, 1];
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (lines[i][j] == '@')
+            {
+                int adjacentCount = 0;
+                
+                for (int k = 0; k < 8; k++)
+                {
+                    if (adjacentCount >= 4)
+                    {
+                        break;
+                    }
+
+                    int ni = i + dx[k];
+                    int nj = j + dy[k];
+                    
+                    if (ni >= 0 && ni < rows && nj >= 0 && nj < cols && lines[ni][nj] == '@')
+                    {
+                        adjacentCount++;
+                    }
+                }
+                
+                if (adjacentCount < 4)
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    
+    return count;
 }
 
 int Part2(string[] lines)
